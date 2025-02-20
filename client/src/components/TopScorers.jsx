c4cde166fcd11e4fe2d1eef0d7b9655caaadd367
Singh-Arekh <singharekh@gmail.com>
2025-02-20 17:24:22 +0530

    changes in top scorer.jsx

--- client/src/components/TopScorers.jsx
+++ client/src/components/TopScorers.jsx
@@ -23,7 +23,7 @@ const TopScorers = () => {
       setLoading(true);
       setError('');
       try {
-        const response = await axios.get(`${import.meta.env.VITE_REACT_APP_BACKEND_URL}/${competition}/scorers`);
+        const response = await axios.get(`${import.meta.env.VITE_REACT_APP_BACKEND_URL}/competitions/${competition}/scorers`);
         setScorers(response.data);
       } catch (error) {
         setError('Error fetching top scorers data.');
